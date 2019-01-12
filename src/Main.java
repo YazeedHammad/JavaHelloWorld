96a8dea75f395171a3262f4889b3765ba976cefc
yazeedhammad <yazeedhammad91@gmail.com>
2019-01-12 19:18:24 +0200

    More Practice / Done

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         MyClass myclass = new MyClass();
         myclass.printMsg("hi there");
         Scanner reader = new Scanner(System.in);
-        for (int i = 0 ; i <3; i++) {
+        for (int i = 0 ; i < 3; i++) {
             System.out.println("Enter a number: ");
             int n = reader.nextInt();
             boolean result = myclass.guessMyNumber(n);
@@ -18,7 +18,23 @@ public class Main {
             }
         }
 
-        reader.close();
         System.out.println("Hello World! My First Java Program!!");
+
+//        int[] num = new int[5];
+//        for ( int i = 0; i < 5; i++) {
+//            System.out.println("Enter  Number:");
+//            num[i] = reader.nextInt();
+//        }
+//        myclass.acceptAndReverse(num);
+//        myclass.acceptAndReversMe(num);
+
+        System.out.println("Enter Name:");
+        String str = reader.next();
+        System.out.println("Enter Counter");
+        int count = reader.nextInt();
+        myclass.repeatString(str, count);
+
     }
+
+
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -8,12 +8,41 @@ public class MyClass {
     public boolean guessMyNumber(int n){
 // TODO: your code here
         Random rn = new Random();
-        rn.nextInt(10 - 0 + 1);
+
         if (rn.nextInt(10 - 0 + 1) == n) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public void acceptAndReverse(int[] array){
+// TODO: your code here
+        for (int i = array.length-1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public void acceptAndReversMe(int[] array){
+// TODO: your code here
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        for (int i = array.length-1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+
+    public void repeatString(String str, int count) {
+// TODO: your code here
+        String name = "";
+        for ( int i = 0; i < count; i++) {
+            name += str;
+
+        }
+        System.out.print(name);
     }
 
 
